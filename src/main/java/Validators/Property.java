785ef964770c6e605e654553cd19690810962d56
1eonID <lpogojev@gmail.com>
2017-08-10 21:32:15 +0300

    Edit class Property and property file

--- src/main/java/Validators/Property.java
+++ src/main/java/Validators/Property.java
@@ -1,19 +1,25 @@
 package Validators;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Properties;
 
 public class Property {
-  Properties properties = new Properties();
-    try {
-    InputStreamReader in = new InputStreamReader(new FileInputStream("prop.properties"));
-    properties.load(in));
-  } catch (FileNotFoundException e) {
-    e.printStackTrace();
-  }
+  private Properties properties = new Properties();
 
+  public Property() {
+    try (FileInputStream fStream = new FileInputStream("prop.properties");
+         InputStreamReader in = new InputStreamReader(fStream)) {
+
+      try {
+        properties.load(in);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
 }
 
